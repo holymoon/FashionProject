5bf0ed6902ed2221d2ea1fc3c17cab80c75ceed0
houra <houra.mehdipour@gmail.com>
2019-12-17 19:18:17 +0330

    add splash screen

--- app/src/main/java/com/example/fashionproject/MainActivity.java
+++ app/src/main/java/com/example/fashionproject/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.fashionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
+
